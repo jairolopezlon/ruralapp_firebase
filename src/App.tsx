6f667b46fa4c2fc0e682e6ca6266c622f5e803dd
Jairo Lopez L <jairolop88@gmail.com>
2023-05-16 20:58:51 -0500

    feat: initial config. tools added (react, sass)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/_main.scss'
+
+function App() {
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', minHeight: '100vh' }}>
+        <img src="/public/images/logo.webp" alt="logo-ruralapp-temporal" width={100} />
+        <h1>RuralApp</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
