5d34d01bcef656106834167fd5dcd3cbbed126b4
Jairo Lopez L <jairolop88@gmail.com>
2023-05-16 23:43:39 -0500

    fix: src path fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './styles/_main.scss'
-
+import '@styles/_main.scss'
 function App() {
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', minHeight: '100vh' }}>
-        <img src="images/logo.webp" alt="logo-ruralapp-temporal" width={100} />
+        <img src="/images/logo.webp" alt="logo-ruralapp-temporal" width={100} />
         <h1>RuralApp</h1>
       </div>
     </>
