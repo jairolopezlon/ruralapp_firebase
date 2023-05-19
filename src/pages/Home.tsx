b743f7f9eaec54fd4a781734c82ba23d753fb80f
Jairo Lopez L <jairolop88@gmail.com>
2023-05-19 00:48:16 -0500

    feat: initial pages added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { MainMenu } from '@src/components'
+
+function Home() {
+  return (
+    <>
+      <MainMenu />
+      <div>NegociosList</div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as NegociosList } from './NegociosList';
+export { default as NegociosProfile } from './NegociosProfile';
+export { default as NotFound } from './NotFound';
